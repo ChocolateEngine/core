faeae88ef8443386c8b671bcf5197ac03e5e1023
p0lyh3dron <thep0lyh3dron@gmail.com>
2022-03-19 23:20:55 -0400

    compile: fix linux

--- shaders/_compile.sh
+++ shaders/_compile.sh
@@ -1,8 +1,8 @@
-glslc basic3d.vert -o basic3d_v.spv
-glslc basic3d.frag -o basic3d_f.spv
+glslc basic3d.vert -o basic3d.vert.spv
+glslc basic3d.frag -o basic3d.frag.spv
 
-glslc basic2d.vert -o basic2d_v.spv
-glslc basic2d.frag -o basic2d_f.spv
+glslc basic2d.vert -o basic2d.vert.spv
+glslc basic2d.frag -o basic2d.frag.spv
 
 glslc unlit.vert -o unlit.vert.spv
 glslc unlit.frag -o unlit.frag.spv
